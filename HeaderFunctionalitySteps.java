dd8ccc63f02d35916d57c82a39f549f4d9f37806
Prajwal-Patil000 <150142656+Prajwal-Patil000@users.noreply.github.com>
2025-11-22 19:07:23 +0530

    Refactor HeaderFunctionalitySteps for improved clarity

--- HeaderFunctionalitySteps.java
+++ HeaderFunctionalitySteps.java
@@ -1,51 +1,102 @@
 package Stepdefinitions;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.openqa.selenium.*;
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+
 import java.time.Duration;
 
 public class HeaderFunctionalitySteps {
-    WebDriver driver;
-    WebDriverWait wait;
+
+    public WebDriver driver;
 
     @Before
     public void setup() {
         System.setProperty("webdriver.chrome.driver",
-                "C:\\Users\\vaish\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+                "C:\\Users\\ADMIN\\Downloads\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.get("https://qamoviesapp.ccbp.tech/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Given("I am on the enter in login")
+    public void imOnTheLoginPage() {
+        driver.get("https://qamoviesapp.ccbp.tech");
+    }
+
+    @When("I enter valid username and password")
+    public void enterUserCredentials() {
         driver.findElement(By.id("usernameInput")).sendKeys("rahul");
         driver.findElement(By.id("passwordInput")).sendKeys("rahul@2021");
+    }
+
+    @And("I click on the login button")
+    public void clickLoginButton() {
         driver.findElement(By.className("login-button")).click();
     }
 
-    @Given("User is logged in on Home page")
-    public void user_is_logged_in_on_home_page() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("home-container")));
+    @Then("I should be redirected to the home page")
+    public void redirectedToHomePage() {
+        String expectedUrl = "https://qamoviesapp.ccbp.tech/";
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe(expectedUrl));
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, expectedUrl, "Home page URL does not match!");
     }
 
-    @When("User clicks on the Popular link in header")
-    public void user_clicks_on_the_popular_link_in_header() {
-        driver.findElement(By.linkText("Popular")).click();
+    @When("I navigate to the Popular page through header")
+    public void navigateToPopularPage() {
+        driver.findElement(By.xpath("//a[text()='Popular']")).click();
     }
 
-    @Then("User should navigate to Popular page")
-    public void user_should_navigate_to_popular_page() {
-        wait.until(ExpectedConditions.urlContains("/popular"));
-        Assert.assertTrue(driver.getCurrentUrl().contains("popular"));
+    @Then("I should be redirected to the Popular page")
+    public void redirectedToPopularPage() {
+        String expectedUrl = "https://qamoviesapp.ccbp.tech/popular";
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe(expectedUrl));
+
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl, "Popular page URL does not match!");
+    }
+
+    @When("I navigate to the Account page through header")
+    public void navigateToAccountPage() {
+        driver.findElement(By.className("avatar-button")).click();
+    }
+
+    @Then("I should be redirected to the Account page")
+    public void redirectedToAccountPage() {
+        String expectedUrl = "https://qamoviesapp.ccbp.tech/account";
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe(expectedUrl));
+
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl, "Account page URL does not match!");
+    }
+
+    @When("I navigate back to the Home page through header")
+    public void navigateBackToHomePage() {
+        driver.findElement(By.xpath("//a[text()='Home']")).click();
+    }
+
+    @Then("I should be redirected back to the Home page")
+    public void redirectedBackToHomePage() {
+        String expectedUrl = "https://qamoviesapp.ccbp.tech/";
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe(expectedUrl));
+
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl, "Failed to navigate back to Home page!");
     }
 
     @After
-    public void teardown() {
-        driver.quit();
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
